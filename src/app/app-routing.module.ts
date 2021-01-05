2af7279bf4211eb4b1f576041a5e803c0301ecd7
seodaeya <seodaeya@gmail.com>
2021-01-05 11:14:44 +0900

    앵귤러 공부

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './home/main.component';
+import { ProductModule } from './products/product.module';
+import { ErrorComponent } from './error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+    MainComponent,
+    ErrorComponent,
+  ],
+  imports: [RouterModule.forRoot(routes), ProductModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/error.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `에러`
+})
+export class ErrorComponent {
+  title = 'error page';
+}
